ed77b492329b3b78a9e488af97ee99a2874a038f
arthurfagundes <arthuurfagundes@gmail.com>
2022-11-08 18:00:37 -0300

    all done

---
+++ ex03/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 03</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+    <form action="index.php" method="POST">
+        <div class="container py-4">
+            <header class="pb-3 mb-4 border-bottom">
+                <h1 class="display-1">Exercício 03</h1>
+            </header>
+            <div class="p-5 mb-4 bg-light rounded-3">
+                <div class="input-group mb-3 text-center">
+                    <span class="input-group-text" id="basic-addon1">Insira uma data:</span>
+                    <input type="date" class="form-control" placeholder="Insira uma data" aria-label="Username" aria-describedby="basic-addon1" name="data">
+                </div>
+                <div class="d-grid gap-2 d-md-block text-start">
+                    <button class="btn btn-primary" type="submit" name="result">Resultado</button>
+                </div>
+                <div class="d-inline-flex">
+                    <?php
+                    if (isset($_POST['result'])) {
+                        $data = $_POST['data'];
+                        $ano = substr($data, 0, 4);
+                        $mes = substr($data, 5, 2);
+                        $dia = substr($data, 8, 2);
+                        echo "<p  class='text-dark fs-3'>Dia: $dia<br>
+                        Mês: $mes<br>
+                        Ano: $ano </p>";
+                    }
+                    ?>
+                </div>
+            </div>
+
+        </div>
+
+
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ ex06/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 06</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+    <form action="index.php" method="POST">
+        <div class="container py-4">
+            <header class="pb-3 mb-4 border-bottom">
+                <h1 class="display-1">Exercício 06</h1>
+            </header>
+            <div class="p-5 mb-4 bg-light rounded-3">
+                <div class="input-group mb-3 text-center">
+
+                    <input type="text" class="form-control" placeholder="Insira uma palavra" aria-label="Username" aria-describedby="basic-addon1" name="url">
+                </div>
+                <div class="d-grid gap-2 d-md-block text-start">
+                    <button class="btn btn-primary" type="submit" name="result">Resultado</button>
+                </div>
+                <div class="d-inline-flex">
+                    <?php
+                    if (isset($_POST['result'])) {
+                        $url = strrev($_POST['url']);
+                        header("Location: $url");
+                    }
+                    // Outro jeito
+                    /*if (isset($_POST['result'])) {
+                        $url = $_POST['url'];
+                        $reverseUrl = strrev($url);
+                        echo "<p  class='text-dark fs-3'>URL: $url<br>
+                        <a  class='text-dark fs-3' href='$reverseUrl'>$reverseUrl</a></p>";
+                    }*/
+                    ?>
+                </div>
+            </div>
+
+        </div>
+
+
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
